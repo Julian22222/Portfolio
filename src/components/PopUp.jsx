1cc0dcff7920cee000cd3b44f78025d57fa64af2
Julian22222 <julik.golovenj@gmail.com>
2023-02-26 17:26:39 +0000

    deploy website

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function PopUp() {
+  return (
+    <div>
+      <form className="form">
+        <label>Name</label>
+        <br></br>
+        <input></input>
+        <br></br>
+        <label>Your email address</label>
+        <br></br>
+        <input></input>
+        <br></br>
+        <label>Subject</label>
+        <br></br>
+        <input></input>
+        <br></br>
+        <label>email</label>
+        <br></br>
+        <input></input>
+        <br></br>
+        <button>Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default PopUp;
