41a65e41de5133b1d9299235050181b50d830ff6
Julian22222 <julik.golovenj@gmail.com>
2024-07-02 14:22:34 +0100

    portfol

--- src/components/Container2.jsx
+++ src/components/Container2.jsx
@@ -1,6 +1,5 @@
 import { FaCheckCircle } from "react-icons/fa";
 // import { useRef, useEffect, useState } from "react";
-import { useInView } from "react-intersection-observer";
 
 const Container2 = () => {
   ////////////////////////////////////////////////////////////////////// animation scrolling blocks
@@ -112,31 +111,6 @@ const Container2 = () => {
           </p>
         </div>
       </div>
-
-      {/*  */}
-      {/* <div className="skill">
-          <div className="outer">
-            <div className="inner">
-              <div id="number">65%</div>
-            </div>
-          </div>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            version="1.1"
-            width="160px"
-            height="160px"
-          >
-            <defs>
-              <linearGradient id="GradientColor">
-                <stop offset="0%" stop-color="#e91e63" />
-                <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle cx="80" cy="80" r="70" stroke-linecap="round" />
-          </svg>
-        </div> */}
-      {/*  */}
     </div>
   );
 };
